84b0e51f2ece2302cf1449322859258853d00255
julianbauer <julian@overnice.de>
2016-03-09 11:05:12 +0100

    index redirect

--- app/main.js
+++ app/main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, Link, hashHistory} from 'react-router';
+import { Router, Route, hashHistory, IndexRedirect} from 'react-router';
 
 import Html from './components/Html/index.jsx';
 import Projekt from './components/Projekt/index.jsx';
@@ -11,6 +11,7 @@ import Kontakt from './components/Kontakt/index.jsx';
 render((
   <Router history={hashHistory}>
     <Route path="/" component={Html}>
+      <IndexRedirect to="/stories" />
       <Route path="projekt" component={Projekt}/>
       <Route path="helfen" component={Helfen}/>
       <Route path="stories" component={Stories}/>
